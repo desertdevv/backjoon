af8cebdeefb5997b3973eb921aca03a50ac3663d
desertdevv <desertdevv@gmail.com>
2025-04-15 23:50:22 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 184 ms, Memory: 17976 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+
+
+
+
+
+    }
+}
