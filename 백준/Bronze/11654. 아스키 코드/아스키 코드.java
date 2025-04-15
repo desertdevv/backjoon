5e4ad1b124d677149a500d164056a49ff3a75bbd
desertdevv <desertdevv@gmail.com>
2025-04-15 22:14:56 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+        char character = input.charAt(0);
+        int num = (int)character;
+        System.out.println(num);
+
+
+
+
+    }
+}
