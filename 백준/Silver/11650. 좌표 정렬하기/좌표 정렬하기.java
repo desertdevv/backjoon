796a5fe128af05bb7aaf574e04cf5f922083a27d
desertdevv <desertdevv@gmail.com>
2025-04-28 23:51:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1732 ms, Memory: 186324 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[][] points = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            points[i][0] = scanner.nextInt();
+            points[i][1] = scanner.nextInt();
+        }
+
+        Arrays.sort(points, (p1, p2) -> {
+            if (p1[0] != p2[0]) {
+                // integer.compare(a,b) a,b 비교해서 -1 0 1 로 나옴
+                return Integer.compare(p1[0], p2[0]);
+            } else {
+                return Integer.compare(p1[1], p2[1]);
+            }
+        });
+
+        for (int[] point : points) {
+            System.out.println(point[0] + " " + point[1]);
+        }
+
+
+    }
+}
+
