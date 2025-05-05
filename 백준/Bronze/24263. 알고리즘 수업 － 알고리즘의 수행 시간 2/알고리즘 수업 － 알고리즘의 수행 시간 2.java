555d609652d3b43c4d7e1b5b422f7c12d3a03b20
desertdevv <desertdevv@gmail.com>
2025-05-05 21:30:14 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        // n번 수행됨. 
+        System.out.println(n);
+
+        // n + sum 의 결과 . 1차항이 존재한다
+        System.out.println(1);
+
+    }
+}
+
